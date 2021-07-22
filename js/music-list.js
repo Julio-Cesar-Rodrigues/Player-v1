98cafbf6373a72aed4bdfdba972fb1711d156ddb
Julio Cesar Rodrigues <62729892+Julio-Cesar-Rodrigues@users.noreply.github.com>
2021-07-21 21:56:54 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,14 @@
+let alllMusic = [
+  {
+    name: 'Wolf Totem (feat. Jacob Shaddix of Papa Roach)',
+    artist: 'The HU, Jacob Shaddix, Papa Roach',
+    img: 'wolf-totem',
+    scr: 'https://soundcloud.com/thehu-music/wolf-totem-feat-jacoby-shaddix'
+  },
+  {
+    name: 'Phenomenal',
+    artist: 'Eminem',
+    img: 'phenomenal',
+    scr: 'https://www.youtube.com/watch?v=Ex55lbcS4wc&ab_channel=EminemMusic'
+  }
+]
